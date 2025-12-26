674e459db3a23911262dc6c2b248215c0f848cdb
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2025-12-26 16:43:19 -0600

    Create text.ts

---
+++ lib/text.ts
@@ -0,0 +1,8 @@
+export function normalizeText(input: string) {
+  return input
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "") // quita acentos
+    .replace(/\s+/g, " ")
+    .trim();
+}
